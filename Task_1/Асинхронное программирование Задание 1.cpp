0ae615012d7e5f046bd9e88ec14502d84ff5865e
Maksim <maks4tmk5423@yandex.ru>
2024-06-18 20:11:39 +0300

    initial commit

---
+++ Task_1/Асинхронное программирование Задание 1.cpp
@@ -0,0 +1,80 @@
+﻿// Асинхронное программирование Задание 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <future>
+
+
+void VecArrayGen(std::vector<int>& vec) {
+	srand(time(0));
+
+	while (vec.size() < 10) {
+		int num = rand() % 10;
+		if (find(vec.begin(), vec.end(), num) == vec.end()) {
+			vec.push_back(num);
+		}
+	}
+}
+
+void FindMinNum(const std::vector<int> vec, int i, std::promise<int> prom)
+{
+	int minNum = i;
+
+	for (int j = i + 1; j < vec.size(); j++)
+	{
+		if (vec[j] < vec[minNum])
+		{
+			minNum = j;
+		}
+	}
+	prom.set_value(minNum);
+};
+
+void SortArray(std::vector<int>& vec)
+{
+	for (int i = 0; i < vec.size() - 1; i++)
+	{
+		std::promise<int> prom;
+		std::future<int> future_res = prom.get_future();
+
+		auto asyncFindMinNum = std::async(std::launch::async, FindMinNum, vec, i, std::move(prom));
+
+		int minNum = future_res.get();
+		if (vec[i] != vec[minNum])
+		{
+			std::swap(vec[i], vec[minNum]);
+		}
+	}
+};
+
+void printVec(const std::vector<int> vec)
+{
+	for (const auto& i : vec)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	std::vector<int> vec;
+
+	VecArrayGen(vec);
+
+	std::cout << "До сортировки: ";
+	printVec(vec);
+
+	SortArray(vec);
+
+	std::cout << "После сортировки: ";
+	printVec(vec);
+}
---
+++ Task_2/Асинхронное программирование Задание 2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+void multiplyVec(int& MultiplierVec)
+{
+	MultiplierVec *= 2;
+}
+
+void For_Each_AsyncRec(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+	auto currSize = std::distance(begin, end);
+
+	if (currSize <= 3)
+	{
+		std::for_each(begin, end, multiplyVec);
+	}
+	else
+	{
+		auto mid = begin;
+		std::advance(mid, currSize / 2);
+		auto asRes = std::async(For_Each_AsyncRec, begin, mid);
+		For_Each_AsyncRec(mid, end);
+
+		asRes.get();
+	}
+}
+
+void PrintArrey(std::vector<int>& vec) {
+	for (int i : vec)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	std::cout << "Было: ";
+	PrintArrey(vec);
+
+	For_Each_AsyncRec(vec.begin(), vec.end());
+
+	std::cout << "Стало: ";
+	PrintArrey(vec);
+
+	return 0;
+}
